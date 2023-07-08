17e7c5889f5ba51ac1b6852fb3036288604444ab
Riley1101 <www.riley.s.eileen@gmail.com>
2023-07-08 12:46:51 -0400

    added error classes for error feedbacks

--- utils/interpreter.ts
+++ utils/interpreter.ts
@@ -1,87 +1,99 @@
-import * as Ast from './ast'
-import { error } from './log'
-import type { LoxObject, } from './types'
-import { TokenType } from './types'
+import * as Ast from "./ast";
+import { error } from "./log";
+import type { LoxObject } from "./types";
+import { TokenType } from "./types";
+import { Token } from "./token";
 
-export class Interpreter implements Ast.ExprVisitor<LoxObject>{
+export class Interpreter implements Ast.ExprVisitor<LoxObject> {
+  private evaluate(expr: Ast.Expr): LoxObject {
+    return expr.accept(this);
+  }
 
-    private evaluate(expr: Ast.Expr): LoxObject {
-        return expr.accept(this)
+  private isEqual(a: LoxObject, b: LoxObject) {
+    if (a == null && b == null) return true;
+    if (a == null) return false;
+    return a === b;
+  }
+
+  interpret(expr: Ast.Expr) {
+    try {
+      let value = this.evaluate(expr);
+      console.log(value);
+    } catch (err) {
+      error(0, "Run time err at interpret");
     }
+  }
 
-    private isEqual(a: LoxObject, b: LoxObject) {
-        if (a == null && b == null) return true;
-        if (a == null) return false
-        return a === b
-    }
+  checkNumberOperand(operator: Token, left: LoxObject, right: LoxObject) {
+    if (typeof left === "number" && typeof right === "number") return;
+    throw new Error(`Operand must be number`);
+  }
 
-    interpret(expr: Ast.Expr) {
-        try {
-            let value = this.evaluate(expr)
-            console.log(value)
-        } catch (err) {
-            error(0, 'Run time err at interpret')
+  visitLiteralExpr(expr: Ast.LiteralExpr): LoxObject {
+    return expr.value;
+  }
+
+  visitBinaryExpr(expr: Ast.BinaryExpr): LoxObject {
+    let left = this.evaluate(expr.left);
+    let right = this.evaluate(expr.right);
+    switch (expr.operator.type) {
+      case TokenType.Minus:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) - (right as number);
+      case TokenType.Slash:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) / (right as number);
+      case TokenType.Star:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) * (right as number);
+      case TokenType.Greater:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) > (right as number);
+      case TokenType.GreaterEqual:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) >= (right as number);
+      case TokenType.Less:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) < (right as number);
+      case TokenType.LessEqual:
+        this.checkNumberOperand(expr.operator, left, right);
+        return (left as number) <= (right as number);
+      case TokenType.EqualEqual:
+        return this.isEqual(left, right);
+      case TokenType.BangEqual:
+        return !this.isEqual(left, right);
+      case TokenType.Plus:
+        this.checkNumberOperand(expr.operator, left, right);
+        if (typeof left === "number" && typeof right === "number") {
+          return left + right;
         }
-    }
-
-    visitLiteralExpr(expr: Ast.LiteralExpr): LoxObject {
-        return expr.value
-    }
-
-    visitBinaryExpr(expr: Ast.BinaryExpr): LoxObject {
-        let left = this.evaluate(expr.left);
-        let right = this.evaluate(expr.right);
-        switch (expr.operator.type) {
-            case TokenType.Minus:
-                return (left as number) - (right as number)
-            case TokenType.Slash:
-                return (left as number) / (right as number)
-            case TokenType.Star:
-                return (left as number) * (right as number)
-            case TokenType.Greater:
-                return (left as number) > (right as number)
-            case TokenType.GreaterEqual:
-                return (left as number) >= (right as number)
-            case TokenType.Less:
-                return (left as number) < (right as number)
-            case TokenType.LessEqual:
-                return (left as number) <= (right as number)
-            case TokenType.EqualEqual:
-                return this.isEqual(left, right)
-            case TokenType.BangEqual:
-                return !this.isEqual(left, right)
-            case TokenType.Plus:
-                if (typeof left === "number" && typeof right === "number") {
-                    return left + right
-                }
-                if (typeof left === "string" && typeof right === "string") {
-                    return left + right
-                }
-                break;
+        if (typeof left === "string" && typeof right === "string") {
+          return left + right;
         }
-        return null;
+        break;
     }
-    /** todo */
-    visitThisExpr(expr: Ast.ThisExpr): LoxObject {
-        return null;
-    }
-    visitSuperExpr(expr: Ast.SuperExpr): LoxObject {
-        return null;
-    }
-    visitUnaryExpr(expr: Ast.UnaryExpr): LoxObject {
-        return null;
-    }
-    visitLogicalExpr(expr: Ast.LogicalExpr): LoxObject {
-        return null;
-    }
-    visitGroupingExpr(expr: Ast.GroupingExpr): LoxObject {
-        return null;
-    }
-    visitVariableExpr(expr: Ast.VariableExpr): LoxObject {
-        return null;
-    }
-    visitAssignmentExpr(expr: Ast.AssignmentExpr): LoxObject {
-        return null;
-    }
-
+    return null;
+  }
+  /** todo */
+  visitThisExpr(expr: Ast.ThisExpr): LoxObject {
+    return null;
+  }
+  visitSuperExpr(expr: Ast.SuperExpr): LoxObject {
+    return null;
+  }
+  visitUnaryExpr(expr: Ast.UnaryExpr): LoxObject {
+    return null;
+  }
+  visitLogicalExpr(expr: Ast.LogicalExpr): LoxObject {
+    return null;
+  }
+  visitGroupingExpr(expr: Ast.GroupingExpr): LoxObject {
+    return null;
+  }
+  visitVariableExpr(expr: Ast.VariableExpr): LoxObject {
+    return null;
+  }
+  visitAssignmentExpr(expr: Ast.AssignmentExpr): LoxObject {
+    return null;
+  }
 }
--- utils/log.ts
+++ utils/log.ts
@@ -1,14 +1,16 @@
+import { Token } from "./token";
+
 /**
  * @param {number} line - report the line number
  * @param {string} message - report the message
  */
-export function error(line: number, message: string): never{
-    report(line, "", message, "error");
-    throw new Error(message);
+export function error(line: number, message: string): never {
+  report(line, "", message, "error");
+  throw new Error(message);
 }
 
 export function info(line: number, message: string) {
-    report(line, "", message, "info");
+  report(line, "", message, "info");
 }
 
 type Log = "error" | "info";
@@ -19,10 +21,84 @@ type Log = "error" | "info";
  * @param {string} message - report the message
  */
 export function report(
-    line: number,
-    where: string,
-    message: string,
-    type: Log
+  line: number,
+  where: string,
+  message: string,
+  type: Log
 ) {
-    console.log(`[${type}] --> [line ${line}] Error ${where}: ${message}`);
+  console.log(`[${type}] --> [line ${line}] Error ${where}: ${message}`);
 }
+
+export class ClientError extends Error {
+  override name = "ClientError";
+  override message: string;
+  constructor(message: string) {
+    super();
+    this.message = message;
+  }
+}
+
+export class SyntaxError extends Error {
+  override name = "SyntaxError";
+  override message: string;
+  line: number;
+  where: string;
+  constructor(message: string, line: number, where: string) {
+    super();
+    this.message = message;
+    this.line = line;
+    this.where = where;
+  }
+}
+
+export class ResolverError extends SyntaxError {
+  override name = "ResolverError";
+}
+
+export class RuntimeError extends Error {
+  override name = "RuntimeError";
+  override message: string;
+  token: Token;
+  constructor(token: Token, message: string) {
+    super();
+    this.token = token;
+    this.message = message;
+  }
+}
+export class CliError extends Error {
+  override name = "CliError";
+  override message: string;
+  constructor(message: string) {
+    super();
+    this.message = message;
+  }
+}
+
+class ErrorReporter {
+  hadCliError = false;
+  hadSyntaxError = false;
+  hadRuntimeError = false;
+
+  report(error: Error): void {
+    let header = "";
+    if (error instanceof SyntaxError && error.line) {
+      header += `[${error.name} (line ${error.line}`;
+      if (error.where) header += ` at ${error.where}`;
+      header += ")";
+    } else if (error instanceof RuntimeError) {
+      header += `[${error.name} (line ${error.token.line})`;
+    } else if (error instanceof CliError) {
+      header += `[${error.name}`;
+    } else {
+      header += "[CliError";
+    }
+    header += "]";
+    console.log(header + " " + error.message);
+
+    if (error instanceof RuntimeError) this.hadRuntimeError = true;
+    else if (error instanceof SyntaxError) this.hadSyntaxError = true;
+    else this.hadCliError = true;
+  }
+}
+
+export const errorReporter = new ErrorReporter();
